c81986ae462b803412a8318195f4ba13192f2bdf
ZeinMansor <zeinmanhasan90@gmail.com>
2022-02-03 17:46:54 +0200

    started product details page

---
+++ layouts/ProductLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Navigation />
+    <ProductMain />
+  </div>
+</template>
+
+<script>
+import Navigation from '../components/Navigation.vue';
+import ProductMain from '../components/ProductMain.vue'
+
+
+export default {
+  components: {
+    Navigation,
+    ProductMain,
+  }
+}
+</script>
+
+<style>
+
+</style>
